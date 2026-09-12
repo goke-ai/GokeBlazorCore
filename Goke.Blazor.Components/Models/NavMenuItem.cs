9380d0e97a90aefbb8e694d1d5f9f6be0405ee98
Goke Oladokun <gokeladokun@hotmail.com>
2026-09-12 15:25:53 +0100

    Add BsIcon, Icons, bs-icons.svg

---
+++ Goke.Blazor.Components/Models/NavMenuItem.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace Goke.Blazor.Components.Models;
+
+public sealed class NavMenuItem
+{
+    public required string Title { get; init; }
+    public required string Href { get; init; }
+    public string? IconCssClass { get; init; }
+    public NavLinkMatch Match { get; init; } = NavLinkMatch.Prefix;
+    public List<NavMenuItem>? Dropdown { get; set; }
+    public bool IsDisabled { get; internal set; }
+    public string Icon { get; set; } = string.Empty;
+    public bool IsDivider { get; set; }
+}
---
+++ Goke.Blazor.Components/ThemeControl.razor.js
@@ -0,0 +1,59 @@
+﻿const storageKey = "theme";
+
+export function Initialize() {
+    // applyStoredTheme();
+
+    document.addEventListener("DOMContentLoaded", applyStoredTheme);
+
+    if (window.Blazor?.addEventListener) {
+        window.Blazor.addEventListener("enhancedload", applyStoredTheme);
+    }
+}
+export function setTheme(theme) {
+    setHtmlThemeAttribute('data-bs-theme', theme);
+}
+
+export function setHtmlThemeAttribute(attribute, value) {
+    document.documentElement.setAttribute(attribute, value);
+}
+
+export function getTheme() {
+    return getHtmlThemeAttribute('data-bs-theme');
+}
+
+export function getHtmlThemeAttribute(attribute) {
+    return document.documentElement.getAttribute(attribute);
+}
+
+export function setStoredTheme(theme) {
+    localStorage.setItem(storageKey, theme);
+}
+
+export function getStoredTheme() {
+    return localStorage.getItem(storageKey);
+}
+
+// Detects current system theme: "light" or "dark"
+export function getSystemTheme() {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
+
+export function applyStoredTheme() {
+    const theme = getStoredTheme() || getSystemTheme();
+    setTheme(theme);
+}
+
+// Allows .NET to subscribe to theme changes
+export function registerThemeChangeHandler(dotNetObjRef) {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    mediaQuery.addEventListener("change", e => {
+        if (getStoredTheme()) {
+            return;
+        }
+
+        const theme = e.matches ? "dark" : "light";
+        setTheme(theme);
+        dotNetObjRef.invokeMethodAsync("OnThemeChanged", theme);
+    });
+}
